1669011810c9a51fef6ba168b4587c10e8f722bd
AyushB21 <ayushbh0612@gmail.com>
2021-12-05 05:15:00 +0530

    Import

---
+++ import.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import os
+import csv
+import time
+from tkinter import filedialog
+import smtplib
+
+
+count, c = 0, 0
+creds = 'emailpass.csv'  # this just sets the creds to 'emailpass.csv'
+rootA = Tk()
